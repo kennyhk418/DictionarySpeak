f1a36a43b4f95f81ec450d846890244c416e1bc9
Kenny <lamkiyinkenny2001@gmail.com>
2017-09-12 10:21:09 -0700

    Dictionary API implemented

--- js/dictionary.js
+++ js/dictionary.js
@@ -1,22 +1,11 @@
 var xml = "http://www.dictionaryapi.com/api/v1/references/sd4/xml/"
-            +"invincible"
+            +"man"
             +"?key=ff737949-9dc1-4d03-81f5-2dcdbe597bdc";
 $(function(){
     $.ajax({
         type: "get",
         url: xml,
         success: function(data){
-            // var xmlhttp = new XMLHttpRequest();
-            // xmlhttp.open("GET", xml, false);
-            // xmlhttp.setRequestHeader("Content-Type", "text/xml");
-            // xmlhttp.send(null);
-
-            // // Store the xml page
-            // var xmlPage = xmlhttp.responseXML;
-            // var temp = xmlPage.getElementsByTagName('entry');
-            // document.write(temp[0].getElementsByTagName('dt').nodeValue);
-            //document.write();
-
             // Meaning of the word
             var def = data.getElementsByTagName('dt');
             for(var i = 0; i < def.length; i++){
@@ -28,9 +17,46 @@ $(function(){
                     result = result.slice(1);
                     // Make the first character upper case
                     result = result.charAt(0).toUpperCase()+ result.slice(1);
-                    document.write(result + "<br>");
+                    //document.write(result + "<br>");
                 }
             }
+
+            // //This is how we retreve information from dictionary now
+            // $data = $(data);
+            // $data.find('entry').each(function(){
+            //     document.write($(this).find("hw").text()+"<br>");
+            //     $current_fl = $(this);
+            //     document.write($(this).find("fl:first").text() + "<br>");
+            //     $(this).find('dt').each(function(){
+            //         document.write($(this).text() + "<br><br>");
+            //     })
+            //     //console.log($(this).find("dt"));
+            // })
+            // console.log($data.find("suggestion"));
+            // $data.find('suggestion').each(function(){
+            //     document.write($(this).text() + "<br>");
+            // })
+
+            //This is how we retreve information from dictionary now
+            $data = $(data);
+            $data.find('entry').each(function(){
+                //document.write($(this).find("hw").text()+"<br>");
+                var string_to_append = "";
+                string_to_append += "<ol class=\"single_word\">";
+                string_to_append += $(this).find("hw").text() + "<br>";
+                string_to_append += $(this).find("fl:first").text()+"</li>";
+
+                $(this).find('dt').each(function(){
+                    string_to_append += "<li>" + $(this).text()+"</li>";
+                })
+                string_to_append += "</ol>"
+                $(".content").append(string_to_append);
+            })
+
+            // If it cant find the word
+            $data.find('suggestion').each(function(){
+                //document.write($(this).text() + "<br>");
+            })
         },
         error: function(xhr, status){
             document.write(status);
